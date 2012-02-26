9956d7e13ce62eca2f2189e8a5a53be2b4345c63
Pete Brousalis <pbrousalis@enova.com>
2012-02-26 15:16:20 -0600

    Screw this branch son

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Egocentric::Application.config.secret_token = '76b1889e3f9a3dd920ca405dd8fbab18c5a00c80456886399fea86fa80159e3bcf14201ec002a3b4aa3b026a6a7a56e678dffde05d5cbea4526538a115d6d9b7'
