ba2b57989f3b92787fa1c4b674ef8c92541c8c9c
Pete Brousalis <pbrousalis@enova.com>
2012-04-06 22:43:22 -0500

    Doing bit things

--- app/models/role.rb
+++
@@ -1,9 +0,0 @@
-class Role < ActiveRecord::Base
-
-  has_and_belongs_to_many :users
-
-  def self.admin
-    find_by_name("admin")
-  end
-
-end
--- spec/models/role_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Role do
-
-  it { should have_db_column(:name).of_type(:string) }
-
-  it { should have_and_belong_to_many(:users) }
-
-end
