22d247c62fc085c34e96db7e418421668c8f3bcd
Pete Brousalis <pbrousalis@enova.com>
2012-04-13 15:18:13 -0500

    Added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
   
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
-  validates_presence_of :email
   validates_presence_of :username, :on => :create
-  validates_uniqueness_of :email
   validates_uniqueness_of :username
+  validates :email, :presence => true, :uniqueness => true
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :on => :create, :message => "is invalid"  
 
   has_many :guides
   has_many :comments
